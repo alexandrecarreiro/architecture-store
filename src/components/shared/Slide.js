9c2b75959a32308be8e28571acd9b6c921121b8a
Alexandre Carreiro <alexandrecarreiro80@gmail.com>
2021-09-21 07:52:50 -0300

    Updated Slide

--- src/components/shared/Slide.js
+++ src/components/shared/Slide.js
@@ -1,122 +1,39 @@
-import styled from "styled-components";
-import { useRef, useState, useCallback, forwardRef } from "react";
-import { motion, useSpring } from "framer-motion";
-import useIsomorphicLayoutEffect from "lib/hooks/useIsomorphicLayoutEffect";
-
-const Area = styled.div`
-  display: flex;
-  overflow: hidden;
-  position: relative;
-  width: 100%;
-`;
-
-const SwipeArea = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  z-index: 100;
-
-  & > div {
-    display: flex;
-    flex: 1;
-
-    &:nth-child(2n + 1) {
-      justify-content: flex-start;
-    }
-
-    &:nth-child(2n) {
-      justify-content: flex-end;
-    }
-  }
-
-  &:hover {
-    & > div > img {
-      display: flex;
-    }
-  }
-`;
-
-const SwipeIcon = styled.img`
-  display: none;
-  height: 32px;
-  opacity: 0.5;
-  transform: rotate(${(props) => (!props.next ? "90" : "-90")}deg);
-  width: 32px;
-`;
-
-const Img = styled.img`
-  position: relative;
-  width: 100%;
-`;
-
-const forwardRefToImg = forwardRef((props, ref) => (
-  <Img ref={ref} {...props} />
-));
-
-forwardRefToImg.displayName = "MotionImg";
-
-const MotionImg = motion(forwardRefToImg);
+import { useState, useRef, useEffect } from "react";
+import { Carousel } from "react-carousel-minimal/next";
+import "react-carousel-minimal/stylesheet";
 
 function Slide({ images }) {
   const slideRef = useRef();
   const [slideWidth, setSlideWidth] = useState(0);
-  const right = useSpring(0);
 
-  const setOffsetWidth = useCallback(() => {
+  useEffect(() => {
     if (slideRef.current) {
       setSlideWidth(slideRef.current.offsetWidth);
-      right.set(0);
     }
-  }, [right]);
+  }, []);
 
-  useIsomorphicLayoutEffect(() => {
-    setOffsetWidth();
-
-    window.addEventListener("resize", setOffsetWidth);
-
-    return () => {
-      window.removeEventListener("resize", setOffsetWidth);
-    };
-  }, [setOffsetWidth]);
-
-  function swipe(type) {
-    let rightPosition = right.get();
-
-    if (type === "next" && rightPosition < slideWidth * (images.length - 1)) {
-      return right.set(rightPosition + slideWidth);
-    }
-
-    if (type === "previous" && rightPosition > 0) {
-      return right.set(rightPosition - slideWidth);
-    }
-
-    return;
-  }
   return (
-    <Area ref={slideRef} slideWidth={slideWidth}>
-      <SwipeArea>
-        <div>
-          <SwipeIcon
-            src="icons/filter-up.png"
-            onClick={() => swipe("previous")}
-            next
-          />
-        </div>
-        <div>
-          <SwipeIcon
-            src="icons/filter-up.png"
-            onClick={() => swipe("next")}
-            previous
-          />
-        </div>
-      </SwipeArea>
-      {images.map(({ url }, key) => (
-        <MotionImg src={url} style={{ right }} key={key} />
-      ))}
-    </Area>
+    <div ref={slideRef}>
+      <Carousel
+        data={images.map((index, key) => ({ image: index.url, key: key }))}
+        time={4000}
+        width="100%"
+        height={slideWidth * 0.66 - 30}
+        radius="10px"
+        captionPosition="bottom"
+        automatic={true}
+        dots={true}
+        pauseIconColor="#222222"
+        pauseIconSize="40px"
+        slideBackgroundColor="#FFF"
+        slideImageFit="contain"
+        thumbnails={true}
+        thumbnailWidth="100px"
+        style={{
+          padding: 15,
+        }}
+      />
+    </div>
   );
 }
 
